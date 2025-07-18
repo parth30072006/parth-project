47fc8f23535aad07f3c3687e9e3fe7d51875fe28
parth30072006 <parthrakholiya6400@gmail.com>
2025-07-18 11:32:48 +0530

    pa

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+@app.route('/products')
+def products():
+    return 'this is product!'
 if __name__ == '__main__':
     app.run(debug=True)
